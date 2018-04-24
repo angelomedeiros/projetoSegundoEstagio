46adc4c5eec62c59e515381496af06e832d5af98
Angelo Medeiros <angelomedeiros3@gmail.com>
2018-04-23 21:35:37 -0300

    Aula 9
    - Implementando as operacoes basicas

---
+++ src/main/java/PilhaSequencial.java
@@ -0,0 +1,61 @@
+public class PilhaSequencial {
+    String[] elementos = new String[100];
+
+    int topo = -1;
+
+    // Verifica se a pilha está vazia
+    public boolean isVazia () {
+        return ( topo == -1 );
+    }
+
+    // Verifica se a pilha está cheia
+    public boolean isCheia () {
+        return ( topo == elementos.length - 1 );
+    }
+
+    // Obtem o total de elementos da pilha
+    public int quantidadeElementos () {
+        return ( topo + 1 );
+    }
+
+    // Obtem o elemento do topo da pilha
+    public String elementoTopo () {
+        if ( isVazia() ) return null;
+        else return elementos[topo];
+    }
+
+    // Insere um na pilha
+    public boolean push ( String valor ) {
+        if ( isCheia() ) return false;
+        else {
+            topo++;
+            elementos[topo] = valor;
+            return true;
+        }
+    }
+
+    // Remove um elemento da pilha
+    public String pop () {
+        if ( isVazia() ) return null;
+        else {
+            String valor = elementos[topo];
+            elementos[topo] = null;
+            topo--;
+            return valor;
+        }
+    }
+
+    // Exibe os elementos da pilha
+//    public void exibirElementos () {
+//        int i = 0;
+//        for ( String elemento : elementos ) {
+//            System.out.printf( "Elemento %d : %s \n", i, elemento);
+//            i++;
+//        }
+//    }
+    public void exibirElementos () {
+        for ( int i = 0; i <= topo; i++ ) {
+            System.out.printf( "Elemento %d : %s \n", i, elementos[i]);
+        }
+    }
+}
---
+++ src/main/java/Principal.java
@@ -0,0 +1,17 @@
+public class Principal {
+    public static void main(String[] args) {
+        PilhaSequencial stack = new PilhaSequencial();
+        stack.push("ana");
+        stack.push("bruno");
+        stack.push("carla");
+        stack.push("daniel");
+        stack.exibirElementos();
+
+        System.out.println("A pilha tem " +  stack.quantidadeElementos() + " elementos\n\n");
+
+        stack.pop();
+        stack.exibirElementos();
+
+        System.out.println("A pilha tem " + stack.quantidadeElementos() + " elementos\n\n");
+    }
+}
